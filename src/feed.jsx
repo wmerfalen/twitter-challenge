21e0322aa4b15fcdcc5dd89e77c2dc6927f8e3f3
William Merfalen <wmerfalen@gmail.com>
2021-09-22 02:18:58 +0000

    visibility component

---
+++ src/feed.jsx
@@ -0,0 +1,9 @@
+
+function CreateTweet(){
+
+    return (
+        <div>
+            <img src="https://pbs.twimg.com/profile_images/1362038709755867145/97I5Rmkx_normal.jpg" className="rounded-profile-pic"/>
+        </div>
+    );
+}
